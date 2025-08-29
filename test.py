04aaffd7d395bda7b3803504163fa72c79777adf
Violetta <murasakitta@gmail.com>
2025-08-29 12:27:16 +0300

    Добавлен локальный проект

---
+++ test.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QApplication, QPushButton, QMainWindow
+
+app = QApplication(sys.argv)
+
+window = QMainWindow()
+window.setFixedSize(QSize(1200, 700))
+window.setWindowTitle("Fineas - financial assistant")
+window.show()
+
+app.exec()
